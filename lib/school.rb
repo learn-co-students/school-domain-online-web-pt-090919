534d8f94d968bf53c3d857e43049176df3bba18a
Jalysa Reason <jalysareason@gmail.com>
2019-10-05 00:15:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  @school = School.new("Bayside High School")
+end 
+
+def roster
+ @school = School.add_student("AC Slater", 9)
+ 
+ @school = School.add_student("Kelly Kapowski", 10)
+ 
+ @school = School.add_student("Screech", 11)
+ 
+ @school = School.roster
+
+end
